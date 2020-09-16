505be1771bb0f60618fca7db547d9b097b255943
Pluveto <i@pluvet.com>
2020-09-16 20:29:49 +0800

    fuck

---
+++ Passionate/Database/QueryFactoryBuilder.cs
@@ -0,0 +1,23 @@
+﻿using SqlKata.Compilers;
+using SqlKata.Execution;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passionate.Database
+{
+    class QueryFactoryBuilder
+    {
+        public static QueryFactory BuildForSqlite(string fileName)
+        {
+            string connStr = $"Data Source={fileName};";
+            var connection = new SQLiteConnection(connStr);
+            var compiler = new SqliteCompiler();
+            var DB = new QueryFactory(connection, compiler);
+            return DB;
+        }
+    }
+}
---
+++ Passionate/MainWindowVM.cs
@@ -0,0 +1,27 @@
+﻿using Passionate.Entity;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passionate
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        public ObservableCollection<CrendentialEntity> CrendentialEntities { get; set; } = new ObservableCollection<CrendentialEntity>();
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindowVM()
+        {
+            Page = 1;
+            PageSize = 25;
+        }
+
+    }
+}
